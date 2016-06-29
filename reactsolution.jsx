3a75aeb948af534ad20f602f355dc6c4a91b7acf
Dominik Enßle <dominik.enssle@web.de>
2016-06-29 14:45:44 +0200

    ListItems & ListTable

--- reactsolution.jsx
+++ reactsolution.jsx
@@ -1,64 +1,9 @@
-var ListItemWrapper = React.createClass({
-  render: function() {
-    return <li>{this.props.data.name}</li>;
-  }
-});
-
-var Lists = React.createClass({
+var ListClass = React.createClass({
   getInitialState: function() {
     return {
       inputValue: "",
       all_lists: this.loadLists()
       };
-  },
-
-  render: function() {
-    return (
-      <div className="listdiv">
-        {this.createNewListInput()}
-        <hr></hr>
-        {this.showLists()}
-      </div>
-    );
-  },
-
-  handleInputChange: function(evt) {
-    this.setState({
-      inputValue: evt.target.value
-    });
-  },
-
-  createNewListInput: function() {
-    return <form id="newListForm">
-      <label>New List:
-      <input type="text" value={this.state.value} onChange={this.handleInputChange}></input>
-        <select ref="typeOption">
-          <option value="standard">Standard</option>
-          <option value="deadline">Deadline</option>
-          <option value="colored">Colored</option>
-        </select>
-      <input type="button" id="createNewListButton" value="Create new list" onClick={this.clickNewList}></input>
-      </label>
-    </form>;
-    },
-
-  showLists: function() {
-    return <ul>
-        {this.state.all_lists.map(function(result) {
-           return <ListItemWrapper key={result.id} data={result}/>;
-        })}
-      </ul>;
-  },
-
-  clickNewList: function() {
-    var listName = this.state.inputValue;
-    var listType = this.refs.typeOption.value;
-    var newList = {id: this.getMilliseconds()+listName+listType, name: listName, type: listType, entries: {}};
-    var allLists = this.state.all_lists;
-    allLists.push(newList);
-    console.log(newList.name + " : " + newList.type);
-    console.log(allLists);
-    this.saveLists();
   },
 
   saveLists: function() {
@@ -75,25 +20,75 @@ var Lists = React.createClass({
     }
     return data;
   },
+
+  render: function() {
+    return (
+      <div className="listdiv">
+        {this.createNewListInput()}
+        <hr></hr>
+          <table>
+          {
+            this.state.all_lists.map(function(list) {
+              return <ListItem list={list} />
+            }.bind(this))
+          }
+          </table>
+      </div>
+    );
+  },
+
+  createNewListInput: function() {
+    return <form id="newListForm">
+      <label>New List:
+      <input type="text" value={this.state.value} onChange={this.handleInputChange}></input>
+        <select ref="typeOption">
+          <option value="standard">Standard</option>
+          <option value="deadline">Deadline</option>
+          <option value="colored">Colored</option>
+        </select>
+      <input type="button" id="createNewListButton" value="Create new list" onClick={this.clickNewList}></input>
+      </label>
+    </form>;
+    },
+
+  handleInputChange: function(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  },
+
+  clickNewList: function() {
+    var listName = this.state.inputValue;
+    var listType = this.refs.typeOption.value;
+    var newList = {id: this.getMilliseconds()+listName+listType, name: listName, type: listType, entries: {}};
+    var allLists = this.state.all_lists;
+    allLists.push(newList);
+    console.log(newList.name + " : " + newList.type);
+    console.log(allLists);
+    this.saveLists();
+  },
+
   getMilliseconds: function() {
     return new Date().getTime();
   }
 });
 
-var CommentList = React.createClass({
+var ListItem = React.createClass({
   render: function() {
-    var commentNodes = this.props.data.map(function(comment) {
-      return (
-        <Comment author={comment.author} key={comment.id}>
-          {comment.text}
-        </Comment>
-      );
-    });
-    return (
-      <div className="commentList">
-        {commentNodes}
-      </div>
-    );
+    var list = this.props.list;
+    return(
+      <tr>
+        <th>{list.name}</th>
+        <td>{list.type}</td>
+        <td><input type="button" value="Select" onClick={this.clickSelect}></input></td>
+        <td><input type="button" value="Delete"onClick={this.clickDelete} ></input></td>
+    </tr>)
+  },
+  clickSelect: function() {
+    console.log(this.props.list.id);
+  },
+  clickDelete: function() {
+    console.log(this.props.list.id);
   }
 });
 
@@ -111,7 +106,7 @@ var App = React.createClass({
   render: function() {
     return (
       <div className="todolist">
-        <Lists />
+        <ListClass />
         <hr></hr>
         <Entries />
       </div>
